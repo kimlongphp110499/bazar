a260834da81d93e62622b825da7ab921691bcb15
kimlongphp110499 <103258383+kimlongphp110499@users.noreply.github.com>
2022-10-03 09:38:13 +0700

    Update PackageController.php

--- api/app/Http/Controllers/PackageController.php
+++ api/app/Http/Controllers/PackageController.php
@@ -26,4 +26,19 @@ class PackageController extends Controller
         $first_package_detail = PackageDetail::where('package_id',$package_first->id)->get();
         return compact('result','package_list','first_package_detail');
     }
+      public function package_create(Request $request){
+        $package = PackageD::create(['service_id' => $request->service_id,
+                                         'name'=>$request->name,]);
+
+        return compact('result');
+    }
+    public function package_properties(Request $request){
+        $package_detail = PackageDetail::create(['package_id' => $request->package_id,
+                                          'max_device'=>$request->max_device,
+                                          'price'=>$request->price,
+                                          'exp_day_time'=>$request->exp_day_time,]);
+
+        return compact('package_detail');
+    }
+    
 }
